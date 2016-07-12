9b2915a7750751ef6b661a38d546701fe199ecda
ivaneh <ivanehreshi@gmail.com>
2016-07-12 13:58:57 +0300

    DataSource example

--- src/main/java/com.gmail.at.ivanehreshi/Db.java
+++ src/main/java/com.gmail.at.ivanehreshi/Db.java
@@ -1,20 +1,39 @@
 package com.gmail.at.ivanehreshi;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
 import javax.rmi.CORBA.Util;
+import javax.sql.ConnectionPoolDataSource;
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.Optional;
 import java.util.concurrent.Exchanger;
 
 public class Db {
+    MysqlDataSource ds;
     String url = "jdbc:mysql://localhost:3306/sample_schema";
+    String dbName = "sample_schema";
     String user = "ivaneh";
     String password = "password";
     Connection connection = null;
     final String SQL_DOMAIN = "SELECT magic_column FROM sample_table";
 
+    public Db() {
+        this.ds = new MysqlDataSource();
+        ds.setDatabaseName(dbName);
+        ds.setUser(user);
+        ds.setPassword(password);
+        ds.setServerName("localhost");
+        ds.setPort(3306);
+    }
+
     public void connect() {
+        /**
+         * DataSource is the preffered method because it is more flexible in the
+         * terms of property changing. It supports connection pools(@ref ConnectionPoolDataSource_
+         */
         try {
-            connection = DriverManager.getConnection(url, user, password);
+            connection = ds.getConnection();
         } catch (SQLException e){
             e.printStackTrace();
             connection = null;

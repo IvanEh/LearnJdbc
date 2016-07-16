c947149a3b5987f5bd61057f81e512c9584ecc48
ivaneh <ivanehreshi@gmail.com>
2016-07-16 20:01:44 +0300

    Implement rowset example

--- src/main/java/com.gmail.at.ivanehreshi/CoffeeDb.java
+++ src/main/java/com.gmail.at.ivanehreshi/CoffeeDb.java
@@ -1,9 +1,14 @@
 package com.gmail.at.ivanehreshi;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.sun.rowset.JdbcRowSetImpl;
+import com.sun.rowset.RowSetFactoryImpl;
 
+import javax.sql.RowSet;
+import javax.sql.rowset.RowSetProvider;
 import java.sql.*;
 import java.util.Optional;
+import java.util.Random;
 
 public class CoffeeDb {
     MysqlDataSource ds;
@@ -11,8 +16,7 @@ public class CoffeeDb {
     String user = "ivaneh";
     String password = "password";
     Connection connection = null;
-    static final String SQL_GET_ALL_COFFEE_ALPHA_ORD = "SELECT name FROM coffee ORDER BY name ASC";
-    static final String SQL_INSERT_COFFEE = "INSERT INTO coffee(name, price) VALUES (?, ?)";
+    static final String SQL_GET_ALL_COFFEE = "SELECT id_coffee, name, price FROM coffee";
 
     public CoffeeDb() {
         this.ds = new MysqlDataSource();
@@ -23,106 +27,128 @@ public class CoffeeDb {
         ds.setPort(3306);
     }
 
-    public void connect() {
-        /**
-         * DataSource is the preffered method because it is more flexible in the
-         * terms of property changing. It supports connection pools(@ref ConnectionPoolDataSource_
-         */
+    public void rowsetFromConnection() {
+        RowSet rowSet = null;
         try {
-            connection = ds.getConnection();
-        } catch (SQLException e){
+            rowSet = new JdbcRowSetImpl(ds.getConnection());
+            rowSet.setCommand(SQL_GET_ALL_COFFEE);
+            rowSet.execute();
+            doStuff(rowSet);
+
+        } catch (SQLException e) {
             e.printStackTrace();
-            connection = null;
-        }
-    }
-
-    public Optional<Connection> getConnection() {
-        return Optional.ofNullable(connection);
-    }
-
-    public void setConnection(Connection connection) {
-        this.connection = connection;
-    }
-
-    public void close() {
-        getConnection().ifPresent(conn -> {
-            try { conn.close(); }
-            catch (SQLException e) {
+        } finally {
+            try {
+                rowSet.close();
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
-        });
-        setConnection(null);
-    }
-
-    public void printAllCoffeeFromLast() {
-        Optional<Connection> maybeConn = getConnection();
-        Connection connection;
-        if(maybeConn.isPresent())
-            connection = maybeConn.get();
-        else
-            return ;
-
-        try(Statement statement = connection.createStatement()) {
-            ResultSet rs = statement.executeQuery(SQL_GET_ALL_COFFEE_ALPHA_ORD);
-            rs.afterLast();
-
-            while (rs.previous()) {
-                System.out.println(rs.getString(1));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 
-    public void dbInfo() {
-        Optional<Connection> maybeConnection = getConnection();
-        Connection connection;
-        if(maybeConnection.isPresent()) {
-            connection = maybeConnection.get();
-        } else {
-            return;
-        }
+    public void rowsetFromRs() {
+        RowSet rowSet = null;
         try {
-            DatabaseMetaData metaData = connection.getMetaData();
+            // resultset should be updatable and scrollable in order to use rowset
+            Statement stmt = ds.getConnection().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                                                                ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = stmt.executeQuery(SQL_GET_ALL_COFFEE);
+            rowSet = new JdbcRowSetImpl(rs);
+            doStuff(rowSet);
 
-            System.out.printf("Support type TYPE_FORWARD_ONLY: %s \n", metaData.supportsResultSetType(ResultSet.TYPE_FORWARD_ONLY));
-            System.out.printf("Support type TYPE_SCROLL_INSENSITIVE:  %s\n", metaData.supportsResultSetType(ResultSet.TYPE_SCROLL_INSENSITIVE));
-            System.out.printf("Support type TYPE_SCROLL_SENSITIVE: %s \n", metaData.supportsResultSetType(ResultSet.TYPE_SCROLL_SENSITIVE));
-
-            System.out.println();
-            System.out.printf("Holdability - hold: %s\n", metaData.supportsResultSetHoldability(ResultSet.HOLD_CURSORS_OVER_COMMIT));
-            System.out.printf("Holdability - close: %s\n", metaData.supportsResultSetHoldability(ResultSet.CLOSE_CURSORS_AT_COMMIT));
-
-            System.out.println();
-            System.out.printf("Concurrency - forward only - read only:  %s\n",
-                    metaData.supportsResultSetConcurrency(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY));
-            System.out.printf("Concurrency - forward only - updatable:  %s\n",
-                    metaData.supportsResultSetConcurrency(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE));
-            System.out.printf("Concurrency - scroll insensitive - updatable:  %s\n",
-                    metaData.supportsResultSetConcurrency(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE));
-            System.out.printf("Concurrency - scroll sensitive - updatable:  %s\n",
-                    metaData.supportsResultSetConcurrency(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE));
-
-            System.out.println();
-            System.out.printf("Transaction isolation level - read committed: %s\n",
-                    metaData.supportsTransactionIsolationLevel(Connection.TRANSACTION_READ_COMMITTED));
-            System.out.printf("Transaction isolation level - read uncommitted: %s\n",
-                    metaData.supportsTransactionIsolationLevel(Connection.TRANSACTION_READ_UNCOMMITTED));
-            System.out.printf("Transaction isolation level - repeatable read: %s\n",
-                    metaData.supportsTransactionIsolationLevel(Connection.TRANSACTION_REPEATABLE_READ));
-            System.out.printf("Transaction isolation level - serializable read: %s\n",
-                    metaData.supportsTransactionIsolationLevel(Connection.TRANSACTION_SERIALIZABLE));
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                rowSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void rowsetFromDefaultCtor() {
+        RowSet rowSet = null;
+        try {
+            // resultset should be updatable and scrollable in order to use rowset
+            rowSet = new JdbcRowSetImpl();
+            rowSet.setUrl("jdbc:mysql://" + "localhost:3306/" + dbName);
+            rowSet.setUsername(user);
+            rowSet.setPassword(password);
+            rowSet.setCommand(SQL_GET_ALL_COFFEE);
+            rowSet.execute();
+            doStuff(rowSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                rowSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void rowsetFromFactory() {
+        RowSet rowSet = null;
+        try {
+            // Works
+            //rowSet = new RowSetFactoryImpl().createJdbcRowSet();
+            // but preferred
+            rowSet = RowSetProvider.newFactory().createJdbcRowSet();
+            rowSet.setUrl("jdbc:mysql://" + "localhost:3306/" + dbName);
+            rowSet.setUsername(user);
+            rowSet.setPassword(password);
+            rowSet.setCommand(SQL_GET_ALL_COFFEE);
+            rowSet.execute();
+            doStuff(rowSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                rowSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void doStuff(RowSet rowSet) throws SQLException {
+        System.out.println("------------");
+        System.out.println("query");
+        while (rowSet.next()) {
+            System.out.printf("%s(%s)\n", rowSet.getString("name"),
+                                        rowSet.getString("price"));
+        }
+        rowSet.beforeFirst();
+        if(rowSet.next()) {
+            System.out.printf("%s(%s)\n", rowSet.getString("name"),
+                    rowSet.getString("price"));
         }
 
+        System.out.println("update");
+        rowSet.afterLast();
+        if(rowSet.previous()) {
+            rowSet.updateDouble("price", new Random().nextDouble() * 20);
+        }
+        rowSet.updateRow();
+
+        System.out.println("insert");
+        rowSet.moveToInsertRow();
+        rowSet.updateString("name", "Random" + new Random().nextInt(7));
+        rowSet.insertRow();
+        System.out.println("------------");
     }
 
 
     public static void main(String[] args) {
         CoffeeDb db = new CoffeeDb();
-        db.connect();
-        db.dbInfo();
-        db.close();
+        db.rowsetFromConnection();
+        System.out.println();
+        db.rowsetFromRs();
+        System.out.println();
+        db.rowsetFromDefaultCtor();
+        System.out.println();
+        db.rowsetFromFactory();
     }
 }
